52b06e38d362855ffe453dad5d90a9be9daf8af7
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-12-25 19:50:19 +0100

    feat: add remove from cart button to products

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,26 +4,38 @@ import { Loader2 } from "lucide-react";
 import styles from "./Shop.module.css";
 
 const Shop = () => {
-  const { addToCart, loading, productsData } = useOutletContext();
+  const { addToCart, loading, productsData, removeFromCart, cart } =
+    useOutletContext();
 
   return loading ? (
     <Loading />
   ) : (
     <div className={styles.grid}>
       {productsData.map((product) => (
-        <ProductCard key={product.id} product={product} addToCart={addToCart} />
+        <ProductCard
+          key={product.id}
+          product={product}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+          cart={cart}
+        />
       ))}
     </div>
   );
 };
 
-const ProductCard = ({ product, addToCart }) => {
+const ProductCard = ({ product, addToCart, removeFromCart, cart }) => {
   const [quantity, setQuantity] = useState(1);
 
   const handleAdd = () => {
     addToCart(product, quantity);
     setQuantity(1);
     alert("Added to cart!");
+  };
+
+  const handleRemove = () => {
+    removeFromCart(product.id);
+    alert("Removed from cart!");
   };
 
   return (
@@ -45,8 +57,15 @@ const ProductCard = ({ product, addToCart }) => {
         <button onClick={() => setQuantity((q) => q + 1)}>+</button>
       </div>
 
-      <button className={styles.addBtn} onClick={handleAdd}>
-        Add to Cart
+      <button
+        className={styles.addBtn}
+        onClick={
+          !cart.some((p) => p.id === product.id) ? handleAdd : handleRemove
+        }
+      >
+        {!cart.some((p) => p.id === product.id)
+          ? "Add to Cart"
+          : "Remove from cart"}
       </button>
     </div>
   );
